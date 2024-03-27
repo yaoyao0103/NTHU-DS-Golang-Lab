9e3003d3db9ec70d1011d3526fb67023bed67053
yaoyao0103 <elites96105@gmail.com>
2024-03-27 12:49:47 +0000

    fix: try to fix the error

--- workerpool/worker_pool.go
+++ workerpool/worker_pool.go
@@ -44,8 +44,9 @@ func (wp *workerPool) Start(ctx context.Context) {
 	//
 	// Starts numWorkers of goroutines, wait until all jobs are done.
 	// Remember to closed the result channel before exit.
+	wp.wg.Add(wp.numWorkers)
 	for i:= 0; i < wp.numWorkers; i++ {
-		wp.wg.Add(1)
+		//wp.wg.Add(1)
 		go wp.run(ctx)
 	}
 	wp.wg.Wait()
